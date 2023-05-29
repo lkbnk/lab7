73708789666fd58653caa11e10ff5ff2ffd0f6c8
lkbnk <lkbnk@outlook.com>
2023-05-30 01:57:43 +0300

    task2 commit

--- 2.py
+++ 2.py
@@ -6,8 +6,8 @@ with Image.open(filename) as img:
 img.show()
 
 img1 = img.resize((img.width // 3, img.height // 3))
-img1.save('1_logo_v.1.jpg')
+img1.save('2_logo_v.1.jpg')
 img2 = img.transpose(Image.FLIP_LEFT_RIGHT)
 img3 = img.transpose(Image.FLIP_TOP_BOTTOM)
-img2.save('1_logo_v.2.jpg')
-img3.save('1_logo_v.3.jpg')
+img2.save('2_logo_v.2.jpg')
+img3.save('2_logo_v.3.jpg')
