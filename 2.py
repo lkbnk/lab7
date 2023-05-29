69f8da2b7617fdc21e501d4f1f5f0c5ffe2c8d0e
lkbnk <lkbnk@outlook.com>
2023-05-30 01:56:24 +0300

    task2 commit

--- 2.py
+++ 2.py
@@ -3,6 +3,7 @@ from PIL import Image
 filename = "1_logo.jpg"
 with Image.open(filename) as img:
     img.load()
+img.show()
 
 img1 = img.resize((img.width // 3, img.height // 3))
 img1.save('1_logo_v.1.jpg')
