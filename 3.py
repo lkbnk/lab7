5f2b1da93a8782705e6bbb5e43e99d070d4b6d11
Ilya <yakubenko@complexis.biz>
2023-05-30 10:56:35 +0300

    task3 commit

--- 3.py
+++ 3.py
@@ -4,6 +4,6 @@ filename = ["3_img1.jpg", "3_img2.jpg", "3_img3.jpg", "3_img4.jpg", "3_img5.jpg"
 for i in filename:
     with Image.open(i) as img:
         img.load()
-        filter_img = img.filter(ImageFilter.FIND_EDGES)
-        filter_img.save("NEW_"+i)
-        filter_img.show()
+        new_img = img.filter(ImageFilter.FIND_EDGES)
+        new_img.save("NEW_"+i)
+        new_img.show()
