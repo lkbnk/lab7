aa91791e4012c7e2ab745aa9897cd17f6f7c4944
Ilya <yakubenko@complexis.biz>
2023-05-30 11:57:13 +0300

    task4 commit

--- 4.py
+++ 4.py
@@ -1,13 +1,14 @@
 from PIL import Image
 
-water = "4_img.png"
-with Image.open(water) as img_water: img_water.load()
+watermark = "4_watermark.png"
+with Image.open(watermark) as img_water: img_water.load()
 
-img_water = Image.open(water).convert('RGBA')
-img_water = img_water.resize((img_water.width // 10, img_water.height // 10))
+img_water = Image.open(watermark)
+img_water = img_water.resize((img_water.width // 2, img_water.height // 2))
 
-filename = "1_logo.jpg"
+filename = "4_img.png"
 with Image.open(filename) as img: img.load()
 
-img.paste(img_water, (300,350),img_water)
+img.paste(img_water, (400, 300), img_water)
 img.save("4_watermark_img.png")
+img.show(img_water)
